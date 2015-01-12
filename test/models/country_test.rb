87dfbe3ccbd17e9954d75c8a383f6eb4676a5d60
Patrick O'Grady <ogradypatrickj@gmail.com>
2015-01-12 14:30:48 -0500

    #4 - add country test data
    -Add countries fixture
    -Add regions fixture
    -Add vicinities fixture
    -Add connections fixture
    
    -Fix country_score validations

--- test/models/country_test.rb
+++ test/models/country_test.rb
@@ -1,7 +1,16 @@
-require 'test_helper'
+require "test_helper"
 
 class CountryTest < ActiveSupport::TestCase
+
+  fixtures :countries, :connections
   # test "the truth" do
   #   assert true
   # end
+
+  def test_neighbors
+    country = Country.find_by(name: "Mexico")
+    assert_equal "Mexico", country.name
+    assert_equal 2, country.neighbors.count
+  end
+
 end
